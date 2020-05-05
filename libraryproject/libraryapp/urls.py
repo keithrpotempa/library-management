69eb5f68617b496e00e6e04619582983fe4203b3
keithrpotempa <keithrpotempa@gmail.com>
2020-05-05 10:42:07 -0500

    finished ch6 detailed view for books and libraries

--- libraryproject/libraryapp/urls.py
+++ libraryproject/libraryapp/urls.py
@@ -6,9 +6,11 @@ app_name = "libraryapp"
 urlpatterns = [
     path('', home, name='home'),
     path('books/', book_list, name='books'),
+    path('books/<int:book_id>/', book_details, name='book'),
     path('book/form', book_form, name='book_form'),
     path('librarians/', librarian_list, name='librarians'),
     path('libraries/', library_list, name='libraries'),
+    path('libraries/<int:library_id>/', library_details, name='library'),
     path('library/form', library_form, name="library_form"),
     path('accounts/', include('django.contrib.auth.urls')),
     path('logout/', logout_user, name='logout'),
--- libraryproject/libraryapp/views/__init__.py
+++ libraryproject/libraryapp/views/__init__.py
@@ -1,9 +1,11 @@
 from .books.list import book_list
+from .books.details import book_details
 from .books.form import book_form
 
 from .librarians.list import librarian_list
 
 from .libraries.list import library_list
+from .libraries.details import library_details
 from .libraries.form import library_form
 
 from .auth.logout import logout_user
