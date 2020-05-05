f01c9ecc78d1e7a53616ec86c54379dbaa7ab201
keithrpotempa <keithrpotempa@gmail.com>
2020-05-05 10:14:56 -0500

    complete up to chapter 5

--- libraryproject/libraryapp/views/__init__.py
+++ libraryproject/libraryapp/views/__init__.py
@@ -1,3 +1,10 @@
 from .books.list import book_list
+from .books.form import book_form
+
 from .librarians.list import librarian_list
+
 from .libraries.list import library_list
+from .libraries.form import library_form
+
+from .auth.logout import logout_user
+from .home import home
